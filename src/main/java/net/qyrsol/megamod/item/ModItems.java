4324d48b80f4548aa7224c3842d741377005d549
Qyrsol <ikomissarov18@gmail.com>
2026-05-14 13:00:13 +0300

    Added steel

---
+++ src/main/java/net/qyrsol/megamod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.qyrsol.megamod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.qyrsol.megamod.MegaMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MegaMod.MOD_ID);
+
+    public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
